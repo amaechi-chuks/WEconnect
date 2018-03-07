d7ef22f0d96b74e4d2d591e07ef6b50f37fca529
chuks <amaechichuks2000@yahoo.com>
2018-03-08 00:29:01 +0100

    commented out APP_TITLE

--- template/js/main.js
+++ template/js/main.js
@@ -4,8 +4,8 @@ const JUMBOTRON_SUB_ELEMENTS = 'What are you waiting for?';
 const COPYRIGHT = 'Chucks Amaechi';
 
 
-document.getElementById("navBarAppTitle").innerHTML = APP_TITLE;
-document.getElementById("footerAppTitle").innerHTML = APP_TITLE;
+//document.getElementById("navBarAppTitle").innerHTML = APP_TITLE;
+//document.getElementById("footerAppTitle").innerHTML = APP_TITLE;
 document.getElementById("jumbotronMainText").innerHTML = JUMBOTRON_MAIN_TEXT;
 document.getElementById("jumbotronSubElements").innerHTML = JUMBOTRON_SUB_ELEMENTS;
 document.getElementById("copyright").innerHTML = COPYRIGHT;
