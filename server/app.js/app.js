309d41353a8a1b354c8de7b09c8d4df5ebd9782d
chuks <amaechichuks2000@yahoo.com>
2018-03-11 15:27:48 +0100

    created express application

---
+++ server/app.js/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+//const businesses = require('./businesses.js');
+//const reviews = require('./reviews.js');
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+
+// Setting up API routes
+app.get('/', (req, res) => res.status(200).send({
+    message: 'Welcome to the Weconnect API!',
+}));
