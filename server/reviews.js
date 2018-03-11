e21a470487296d399c5c6683303b703a693671aa
chuks <amaechichuks2000@yahoo.com>
2018-03-12 00:59:02 +0100

    added dummy data for reviews

---
+++ server/reviews.js
@@ -0,0 +1,14 @@
+const reviews = [{
+    businessid : 1,
+    comments : ['Nice people to trade with']
+},
+{
+    businessid : 2,
+    comments : ['I Love your products and customer support']
+},
+{
+    businessid : 3,
+    comments : ['It will be nicer if you treat your customers with a little more respect']
+}
+];
+module.exports = reviews;
