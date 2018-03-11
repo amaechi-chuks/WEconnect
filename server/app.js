ce5a4a3fd8d6fad2ddf5b1be7030090f565b940f
chuks <amaechichuks2000@yahoo.com>
2018-03-12 00:41:05 +0100

    fix up error in the route

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-//const businesses = require('./businesses.js');
+const businesses = require('./businesses.js');
 //const reviews = require('./reviews.js');
 // Set up the express app
 const app = express();
@@ -27,7 +27,7 @@ app.get('/', (req, res) => res.status(200).send({
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('/businesses/:businessid', (req, res) => {
     let businessid = parseInt(req.params.businessid, 10);
-    console.log(businessid);
+    //console.log(businessid);
     let result = businesses.filter(mybiz => mybiz.businessid === businessid)[0];
 
     if ((!result) ?  res .sendStatus(404) : res.send(result));
