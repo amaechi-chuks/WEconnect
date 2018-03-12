a7b32a1ffba8b484dbe9fe4d8c4544722371b074
chuks <amaechichuks2000@yahoo.com>
2018-03-12 12:01:53 +0100

    added apost and delete route into the controller

--- server/app.js
+++ server/app.js
@@ -44,29 +44,84 @@ app.get('/businesses/:businessid/reviews', (req, res) => {
     known.forEach((memm) => {
         res.send(memm.comments);
     });
+});
 
-    // GET all businesses
+// GET all businesses
 app.get('/businesses', (req, res) => res.status(200).send(businesses));
 
 // GET all reviews
 app.get('/reviews', (req, res) => res.status(200).send(reviews));
-
+  
 // POST a Business for Weconnect
 app.post('/businesses', (req, res) => {
     const newBiz = req.body;
-    console.log(newBiz.businessid);
+    //console.log(newBiz.businessid);
     let found = businesses.find((check) => {
         return check.businessid === newBiz.businessid;
-    })
-    if(found){
+    });
+    if(found) {
         return res.sendStatus(400); 
+    } else {
+        businesses.push(newBiz);
     }
-    else{
-    businesses.push(newBiz);
-    }
+        
     // return posted businesses
     const result = businesses.filter(newBusiness => newBusiness.businessid === newBiz.id)[0];
     res.send(newBiz);
 });
-   
+
+// Post a review
+app.post('/businesses/:businessid/reviews', (req, res) => {
+    let bizid = parseInt(req.params.businessid, 10);
+    const newReview = req.body;
+    let known = reviews.filter((now) => {
+        return now.businessid === bizid;
+    });
+    res.send(found);
+    // if(found){
+    //     reviews.push(newBiz);
+    // }
+    // else{
+    //  return res.sendStatus(400); 
+    // }
+    // if (!item.id) {
+    //     return res.sendStatus(500);
+    // }
+
+    // businesses.review.push(item.review);
+    // // return posted businesses
+    // const result = businesses.filter(newbusinesses => newbusinesses.id === item.id)[0];
+    // res.send(result);
+});
+// PUT Method
+app.put('/api/businesses/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const oldbusinesses = businesses.filter(r => r.id === id)[0];
+
+    if (!oldbusinesses) {
+        const item = req.body;
+        item.id = id;
+        businesses.push(item);
+        // res.setHeader('Location', `/api/businesses/) + {id}`);
+        res.sendStatus(201);
+    } else {
+        oldbusinesses.name = req.body.name;
+        oldbusinesses.id = req.body.id;
+        oldbusinesses.reviews = req.body.reviews;
+        res.sendStatus(204);
+    }
+});
+// Delete Method
+app.delete('/businesses/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const existingItem = businesses.filter(r => r.id === id)[0];
+
+    if (!existingItem) {
+        return res.sendStatus(404);
+    }
+
+    // const newbusinesses = businesses.filter(r => r.id !== id);
+    // res.sendStatus(204);
+});
+
 module.exports = app;
