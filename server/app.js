98d5f200ddc2bc44242f30b7ec8c7b6ce9634b9b
chuks <amaechichuks2000@yahoo.com>
2018-03-17 12:25:16 +0100

    removed comments

--- server/app.js
+++ server/app.js
@@ -26,10 +26,7 @@ app.get('/bus', (req, res) => {
     if (req.query) {
         res.send(req.query);
     }
-
-
 });
-
 
 
 let businesses = require('./route/businesses.js');
@@ -37,11 +34,6 @@ let auth = require('./route/user.js');
 
 app.use('/auth', auth);
 app.use('/businesses', businesses);
-
-
-// GET all reviews
-// app.get('/reviews', (req, res) => res.status(200).send(reviews));
-
 // PUT Method
 app.put('/api/businesses/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
@@ -60,34 +52,7 @@ app.put('/api/businesses/:id', (req, res) => {
         res.sendStatus(204);
     }
 });
-// Post a review
-app.post('/businesses/:businessid/reviews', (req, res) => {
-    let bizid = parseInt(req.params.businessid, 10);
-    const newReview = req.body;
-    let known = reviews.filter((now) => {
-        return now.businessid === bizid;
-    });
-    res.send(found);
 
-});
-// PUT Method
-app.put('/api/businesses/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const oldbusinesses = businesses.filter(r => r.id === id)[0];
-
-    if (!oldbusinesses) {
-        const item = req.body;
-        item.id = id;
-        businesses.push(item);
-        // res.setHeader('Location', `/api/businesses/) + {id}`);
-        res.sendStatus(201);
-    } else {
-        oldbusinesses.name = req.body.name;
-        oldbusinesses.id = req.body.id;
-        oldbusinesses.reviews = req.body.reviews;
-        res.sendStatus(204);
-    }
-});
 // Delete Method
 app.delete('/businesses/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
