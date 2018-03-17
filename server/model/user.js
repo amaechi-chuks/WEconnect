d6aefbb7038c5c61169b1657e3a2f88d674a2e61
chuks <amaechichuks2000@yahoo.com>
2018-03-17 13:14:57 +0100

    indented the user dummy data

---
+++ server/model/user.js
@@ -0,0 +1,16 @@
+let users = [{
+    name : 'chuks log',
+    userId : 1,
+    userName : 'chuks_nlog',
+    email : 'amaechichuks2000@yahoo.com',
+    pass : 'daddy_freeze'
+},
+{
+    name : 'Wale Adewale',
+    userId : 2,
+    userName : 'walecxy',
+    email : 'wale_adebayo220@yahoo.com',
+    pass : 'tara_preye'
+}];
+
+module.exports = users;
