c1c4833c2699183d69750df6458f30ae5bce9f3a
chuks <amaechichuks2000@yahoo.com>
2018-03-17 18:31:47 +0100

    modified the user dummy data

--- server/model/user.js
+++ server/model/user.js
@@ -3,14 +3,14 @@ let users = [{
     userId : 1,
     userName : 'chuks_nlog',
     email : 'amaechichuks2000@yahoo.com',
-    pass : 'daddy_freeze'
+    password : 'daddy_freeze'
 },
 {
     name : 'Wale Adewale',
     userId : 2,
     userName : 'walecxy',
     email : 'wale_adebayo220@yahoo.com',
-    pass : 'tara_preye'
+    password : 'tara_preye'
 }];
 
 module.exports = users;
