026843d614eeeced7fafef225644010e61b77cee
chuks <amaechichuks2000@yahoo.com>
2018-03-19 20:23:24 +0100

    moved template files to root folder

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const APP_TITLE = 'WeConnect';
+const JUMBOTRON_MAIN_TEXT = 'Connecting Business';
+const JUMBOTRON_SUB_ELEMENTS = 'What are you waiting for?';
+const COPYRIGHT = 'Chucks Amaechi';
+
+
+//document.getElementById("navBarAppTitle").innerHTML = APP_TITLE;
+//document.getElementById("footerAppTitle").innerHTML = APP_TITLE;
+document.getElementById("jumbotronMainText").innerHTML = JUMBOTRON_MAIN_TEXT;
+document.getElementById("jumbotronSubElements").innerHTML = JUMBOTRON_SUB_ELEMENTS;
+document.getElementById("copyright").innerHTML = COPYRIGHT;
